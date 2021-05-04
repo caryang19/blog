c9f8ba89dcde874c304e808bd1865ade2f2bcd61
Olumide Olamilekan <51947356+caryang19@users.noreply.github.com>
2021-05-04 12:12:08 -0800

    Django Blog Web App
    Django Blog Web App

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 
-class post(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200)
     authour = models.ForeignKey('auth.User',on_delete=models.CASCADE,)
     body = models.TextField()
 
-    def _str_(self):
+    def __str__(self):
         return self.title
